7e63824e3ba36c75e32678113d3c6d52c4a82f1d
David Hunsdon <DaveH1981@users.noreply.github.com>
2024-10-16 11:30:43 -0700

    Update scripts.js

--- interactive-css-page/scripts.js
+++ interactive-css-page/scripts.js
@@ -33,3 +33,69 @@ coolFeatureTwo.addEventListener("mouseover", event => {
   event.target.style.color = "brown";
   event.target.textContent = "Hmmm...should you do it?";
 });
+
+coolFeatureTwo.addEventListener("mouseout", event => {
+  event.target.style.backgroundColor = "darkblue";
+  event.target.style.color = "#f2f2f2";
+  event.target.textContent = "Cool Feature 2";
+});
+
+const coolFeatureThree = document.getElementById("coolFeatureThree");
+
+coolFeatureThree.addEventListener("click", event => {
+  event.target.style.backgroundColor = "limegreen";
+  event.target.style.color = "#f2f2f2";
+  event.target.textContent = "Hey There!";
+});
+
+coolFeatureThree.addEventListener("mouseover", event => {
+  event.target.style.backgroundColor = "gold";
+  event.target.style.color = "brown";
+  event.target.textContent = "Hmmm...should you do it?";
+});
+
+coolFeatureThree.addEventListener("mouseout", event => {
+  event.target.style.backgroundColor = "darkblue";
+  event.target.style.color = "#f2f2f2";
+  event.target.textContent = "Cool Feature 3";
+});
+
+const coolFeatureFour = document.getElementById("coolFeatureFour");
+
+coolFeatureFour.addEventListener("click", event => {
+  event.target.style.backgroundColor = "limegreen";
+  event.target.style.color = "#f2f2f2";
+  event.target.textContent = "Hey There!";
+});
+
+coolFeatureFour.addEventListener("mouseover", event => {
+  event.target.style.backgroundColor = "gold";
+  event.target.style.color = "brown";
+  event.target.textContent = "Hmmm...should you do it?";
+});
+
+coolFeatureFour.addEventListener("mouseout", event => {
+  event.target.style.backgroundColor = "darkblue";
+  event.target.style.color = "#f2f2f2";
+  event.target.textContent = "Cool Feature 4";
+});
+
+const coolFeatureFive = document.getElementById("coolFeatureFive");
+
+coolFeatureFive.addEventListener("click", event => {
+  event.target.style.backgroundColor = "limegreen";
+  event.target.style.color = "#f2f2f2";
+  event.target.textContent = "Hey There!";
+});
+
+coolFeatureFive.addEventListener("mouseover", event => {
+  event.target.style.backgroundColor = "gold";
+  event.target.style.color = "brown";
+  event.target.textContent = "Hmmm...should you do it?";
+});
+
+coolFeatureFive.addEventListener("mouseout", event => {
+  event.target.style.backgroundColor = "darkblue";
+  event.target.style.color = "#f2f2f2";
+  event.target.textContent = "Cool Feature 5";
+});
